9253a9532112b58c8e6d24bd95d97dea23399e6e
Piotr Gabryanczyk <piotrga@gmail.com>
2017-07-19 14:46:38 +0100

    REST

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,28 @@
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+
 object Main extends App{
+
+
+    implicit val actorSystem = ActorSystem("system")
+    implicit val actorMaterializer = ActorMaterializer()
+
+
+    val route =
+      pathSingleSlash {
+        get {
+          complete {
+            "Hello world "+System.currentTimeMillis()
+          }
+        }
+      }
+    Http().bindAndHandle(route,"localhost",8080)
+
+    println("server started at 8080")
+
 	println("Version 3.0")
 	while(true){
 		println(".")
